159ab0c53c6dec436be3008648782b67da21f509
avifyulya <avifyulya23@gmail.com>
2026-06-26 16:15:02 +0700

    baru

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('apikey')->group(function () {
-    Route::get('/v1/transactions', [TransactionController::class, 'index']);
-    Route::get('/v1/transactions/{id}', [TransactionController::class, 'show']);
-    Route::post('/v1/transactions', [TransactionController::class, 'store']);
-    Route::put('/v1/transactions/{id}', [TransactionController::class, 'update']);
+Route::middleware('apikey')->prefix('v1')->group(function () {
+    Route::get('/transactions', [TransactionController::class, 'index']);
+    Route::get('/transactions/{id}', [TransactionController::class, 'show']);
+    Route::post('/transactions', [TransactionController::class, 'store']);
+    Route::put('/transactions/{id}', [TransactionController::class, 'update']);
 });
