0c1facc31faa9a546368cbb7b608b2cd21948dca
avifyulya <avifyulya23@gmail.com>
2026-06-26 16:37:38 +0700

    perubahan

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('apikey')->prefix('v1')->group(function () {
-    Route::get('/transactions', [TransactionController::class, 'index']);
-    Route::get('/transactions/{id}', [TransactionController::class, 'show']);
-    Route::post('/transactions', [TransactionController::class, 'store']);
-    Route::put('/transactions/{id}', [TransactionController::class, 'update']);
+Route::middleware('apikey')->group(function () {
+    Route::prefix('v1')->group(function () {
+        Route::get('/transactions', [TransactionController::class, 'index']);
+        Route::get('/transactions/{id}', [TransactionController::class, 'show']);
+        Route::post('/transactions', [TransactionController::class, 'store']);
+        Route::put('/transactions/{id}', [TransactionController::class, 'update']);
+    });
 });
